2dc2beea8da6b8e6144aede342c9b6eedd84f5f1
Enzo Honorato <enzohonoratobr@gmail.com>
2023-02-27 14:54:16 -0300

    Problema 3 resolvido utilizando os dados do XML fornecido. A biblioteca externa XStream foi utilizada no processo de desserializar o XML para objetos em memória.

--- src/Pergunta3.java
+++ src/Pergunta3.java
@@ -1,28 +1,82 @@
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+import com.thoughtworks.xstream.security.AnyTypePermission;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+class Row {
+    public int dia;
+    public double valor;
+
+    public Row(int dia, double valor) {
+        this.dia = dia;
+        this.valor = valor;
+    }
+
+}
+
 public class Pergunta3 {
     public static void main(String[] args) {
-        int[] faturamentos = new int[]{14000, 0, 4000, 15000, 16000, 0, 17000, 18000, 19000};
+
+        XStream stream = new XStream(new DomDriver());
+        stream.addPermission(AnyTypePermission.ANY);
+
+        String linha, xmlString;
+        StringBuilder xml;
+        List<Row> rows = new ArrayList<>();
+
+        File file = new File("src/dados.xml");
+        FileReader fileReader;
+        BufferedReader bufferedReader;
+
+        try {
+            fileReader = new FileReader(file);
+            bufferedReader = new BufferedReader(fileReader);
+
+            while (bufferedReader.ready()) {
+                xml = new StringBuilder("");
+
+                for (int i = 0; i < 4; i++) {
+                    linha = bufferedReader.readLine();
+                    xml.append(linha);
+                }
+
+                xmlString = xml.toString();
+
+                Row row = (Row) stream.fromXML(xmlString);
+
+                rows.add(row);
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
         int numeroDiasFaturamentoMaiorQueZero = 0, somaFaturamentos = 0, numeroDiasFaturamentoMaiorQueMedia = 0;
-        int menorFaturamento = faturamentos[0], maiorFaturamento = faturamentos[0];
+        int diaMenorFaturamento = 0, diaMaiorFaturamento = 0;
 
-        float mediaFaturamento;
+        double mediaFaturamento;
 
-        for (int i = 0; i < faturamentos.length; i++) {
-            if (faturamentos[i] > 0) {
+        for (Row row : rows) {
+            if (row.valor > 0) {
                 numeroDiasFaturamentoMaiorQueZero++;
-                somaFaturamentos += faturamentos[i];
-                if (faturamentos[i] < menorFaturamento) menorFaturamento = faturamentos[i];
-                if (faturamentos[i] > maiorFaturamento) maiorFaturamento = faturamentos[i];
+                somaFaturamentos += row.valor;
+                if (row.valor < rows.get(diaMenorFaturamento).valor) diaMenorFaturamento = row.dia - 1;
+                if (row.valor > rows.get(diaMaiorFaturamento).valor) diaMaiorFaturamento = row.dia - 1;
             }
         }
 
-        mediaFaturamento = (float) somaFaturamentos / numeroDiasFaturamentoMaiorQueZero;
+        mediaFaturamento = (double) somaFaturamentos / numeroDiasFaturamentoMaiorQueZero;
 
-        for (int i = 0; i < faturamentos.length; i++) {
-            if (faturamentos[i] > mediaFaturamento) numeroDiasFaturamentoMaiorQueMedia++;
+        for (Row row : rows) {
+            if (row.valor > mediaFaturamento) numeroDiasFaturamentoMaiorQueMedia++;
         }
 
-        System.out.println("Menor faturamento: " + menorFaturamento);
-        System.out.println("Maior faturamento: " + maiorFaturamento);
+        System.out.println("Menor faturamento: " + rows.get(diaMenorFaturamento).valor + " no dia " + (diaMenorFaturamento + 1));
+        System.out.println("Maior faturamento: " + rows.get(diaMaiorFaturamento).valor + " no dia " + (diaMaiorFaturamento + 1));
         System.out.println("Média de faturamento: " + mediaFaturamento);
         System.out.println("Número de dias com faturamento maior que a média: " + numeroDiasFaturamentoMaiorQueMedia);
 
